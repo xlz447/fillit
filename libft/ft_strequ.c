89624be3d6630e1d1adfa924b2d3b6c0b7373626
Xuan Zhu <xzhu@e1z3r6p25.42.us.org>
2018-07-18 16:46:59 -0700

    final verison

--- libft/ft_strequ.c
+++ libft/ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: xzhu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/10 13:56:45 by xzhu              #+#    #+#             */
-/*   Updated: 2018/07/10 13:56:47 by xzhu             ###   ########.fr       */
+/*   Updated: 2018/07/18 15:05:56 by xzhu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ int		ft_strequ(char const *s1, char const *s2)
 		return (1);
 	if (!s1 || !s2)
 		return (0);
-	return (!strcmp(s1, s2));
+	return (!ft_strcmp(s1, s2));
 }
--- libft/ft_strnequ.c
+++ libft/ft_strnequ.c
@@ -18,5 +18,5 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 		return (1);
 	if (!s1 || !s2)
 		return (0);
-	return (!strncmp(s1, s2, n));
+	return (!ft_strncmp(s1, s2, n));
 }
